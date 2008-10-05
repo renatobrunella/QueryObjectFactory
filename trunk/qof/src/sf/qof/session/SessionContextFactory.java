f7218b0750da26c90ff160df5416431b3585a518
brunella <brunella@c553a772-16ad-418b-8912-1e50f42fc919>
2008-10-05 11:38:27 +0000

    Added SessionConnectionHandler
    git-svn-id: http://svn.code.sf.net/p/qof-jdbc/code@18 c553a772-16ad-418b-8912-1e50f42fc919

--- trunk/qof/src/sf/qof/session/SessionContextFactory.java
+++ trunk/qof/src/sf/qof/session/SessionContextFactory.java
@@ -77,7 +77,7 @@ public class SessionContextFactory {
    * @return the session context
    */
   public static SessionContext getContext() {
-	return getContext(SessionContext.DEFAULT_CONTEXT_NAME);
+    return getContext(SessionContext.DEFAULT_CONTEXT_NAME);
   }
 
   /**
@@ -87,12 +87,12 @@ public class SessionContextFactory {
    * @return the session context
    */
   public synchronized static SessionContext getContext(String contextName) {
-	SessionContext sessionContext = sessionContextMap.get(contextName);
-	if (sessionContext == null) {
-	  sessionContext = new DefaultSessionContext(contextName);
-	  sessionContextMap.put(contextName, sessionContext);
-	}
-	return sessionContext;
+    SessionContext sessionContext = sessionContextMap.get(contextName);
+    if (sessionContext == null) {
+      sessionContext = new DefaultSessionContext(contextName);
+      sessionContextMap.put(contextName, sessionContext);
+    }
+    return sessionContext;
   }
 
   /**
@@ -101,7 +101,7 @@ public class SessionContextFactory {
    * @param dataSource  the data source
    */
   public static void setDataSource(DataSource dataSource) {
-	setDataSource(SessionContext.DEFAULT_CONTEXT_NAME, dataSource);
+    setDataSource(SessionContext.DEFAULT_CONTEXT_NAME, dataSource);
   }
 
   /**
@@ -111,7 +111,34 @@ public class SessionContextFactory {
    * @param dataSource   the data source
    */
   public static void setDataSource(String contextName, DataSource dataSource) {
-	((DefaultSessionContext) getContext(contextName)).setDataSource(dataSource);
+    ((DefaultSessionContext) getContext(contextName)).setDataSource(dataSource);
+  }
+
+  /**
+   * Registers a <code>SessionConnectionHandler</code> with the default session context.
+   * 
+   * The default handler is <code>DefaultSessionConnectionHandler</code>.
+   * 
+   * @param sessionConnectionHandler the session connection handler
+   * 
+   * @see DefaultSessionConnectionHandler
+   */
+  public static void setSessionConnectionHandler(SessionConnectionHandler sessionConnectionHandler) {
+    setSessionConnectionHandler(SessionContext.DEFAULT_CONTEXT_NAME, sessionConnectionHandler);
+  }
+  
+  /**
+   * Registers a <code>SessionConnectionHandler</code> with the specified session context.
+   * 
+   * The default handler is <code>DefaultSessionConnectionHandler</code>.
+   * 
+   * @param contextName  the session context name
+   * @param sessionConnectionHandler the session connection handler
+   * 
+   * @see DefaultSessionConnectionHandler
+   */
+  public static void setSessionConnectionHandler(String contextName, SessionConnectionHandler sessionConnectionHandler) {
+    ((DefaultSessionContext) getContext(contextName)).setSessionConnectionHandler(sessionConnectionHandler);
   }
 
   /**
@@ -122,123 +149,113 @@ public class SessionContextFactory {
    */
   protected static class DefaultSessionContext implements SessionContext {
 
-	private DataSource dataSource;
-	private String contextName;
-
-	private ThreadLocal<Session> sessionThreadLocal = new ThreadLocal<Session>() {
-	  protected synchronized Session initialValue() {
-		return new Session();
-	  }
-	};
-
-	private DefaultSessionContext(String contextName) {
-	  this.contextName = contextName;
-	}
-
-	private void setDataSource(DataSource dataSource) {
-	  this.dataSource = dataSource;
-	}
-
-	public Connection getConnection() {
-	  Session session = sessionThreadLocal.get();
-	  if (session.getState() == SessionState.STOPPED) {
-		throw new IllegalStateException(
-			"Session is not running in thread for context " + contextName);
-	  } else {
-		return session.getConnection();
-	  }
-	}
-
-	public UserTransaction getUserTransaction() {
-	  Session session = sessionThreadLocal.get();
-	  if (session.getState() == SessionState.STOPPED) {
-		throw new IllegalStateException(
-			"Session is not running in thread for context " + contextName);
-	  } else {
-		return session.getUserTransaction();
-	  }
-	}
-
-	public void startSession() throws SystemException {
-	  Session session = sessionThreadLocal.get();
-	  if (session.getState() == SessionState.RUNNING) {
-		throw new IllegalStateException(
-			"Session already running in thread for context " + contextName);
-	  } else {
-		if (dataSource == null) {
-		  throw new SystemException("No data source defined for context "
-			  + contextName);
-		}
-		Connection connection;
-		try {
-		  connection = dataSource.getConnection();
-		  if (connection == null) {
-		    throw new SQLException("DataSource returned null connection");
-		  }
-		  connection.setAutoCommit(false);
-		} catch (SQLException e) {
-		  throw new SystemException(e);
-		}
-		session.setConnection(connection);
-		session.setUserTransaction(new DefaultUserTransaction(connection));
-		session.setState(SessionState.RUNNING);
-	  }
-	}
-
-	public void stopSession() throws SystemException {
-	  Session session = sessionThreadLocal.get();
-	  if (session.getState() == SessionState.STOPPED) {
-		throw new IllegalStateException(
-			"Session is not running in thread for context " + contextName);
-	  } else {
-		session.setState(SessionState.STOPPED);
-		try {
-		  ((DefaultUserTransaction) session.getUserTransaction()).close();
-		  session.setUserTransaction(null);
-		  session.getConnection().close();
-		  session.setConnection(null);
-		} catch (SQLException e) {
-		  throw new SystemException(e);
-		}
-	  }
-	}
-	
-	
+    private final static SessionConnectionHandler DEFAULT_SESSION_CONNECTION_HANDLER =
+      new DefaultSessionConnectionHandler();
+  
+    private DataSource dataSource;
+    private String contextName;
+    private SessionConnectionHandler sessionConnectionHandler;
+  
+    private ThreadLocal<Session> sessionThreadLocal = new ThreadLocal<Session>() {
+      protected synchronized Session initialValue() {
+        return new Session();
+      }
+    };
+  
+    private DefaultSessionContext(String contextName) {
+      this.contextName = contextName;
+      this.sessionConnectionHandler = DEFAULT_SESSION_CONNECTION_HANDLER;
+    }
+  
+    private void setDataSource(DataSource dataSource) {
+      this.dataSource = dataSource;
+    }
+    
+    private void setSessionConnectionHandler(SessionConnectionHandler sessionConnectionHandler) {
+      this.sessionConnectionHandler = sessionConnectionHandler;
+    }
+  
+    public Connection getConnection() {
+      Session session = sessionThreadLocal.get();
+      if (session.getState() == SessionState.STOPPED) {
+        throw new IllegalStateException("Session is not running in thread for context " + contextName);
+      } else {
+        return session.getConnection();
+      }
+    }
+  
+    public UserTransaction getUserTransaction() {
+      Session session = sessionThreadLocal.get();
+      if (session.getState() == SessionState.STOPPED) {
+        throw new IllegalStateException("Session is not running in thread for context " + contextName);
+      } else {
+      return session.getUserTransaction();
+      }
+    }
+  
+    public void startSession() throws SystemException {
+      Session session = sessionThreadLocal.get();
+      if (session.getState() == SessionState.RUNNING) {
+        throw new IllegalStateException("Session already running in thread for context " + contextName);
+      } else {
+        if (dataSource == null) {
+          throw new SystemException("No data source defined for context " + contextName);
+      }
+      Connection connection = sessionConnectionHandler.getConnection(dataSource);
+      session.setConnection(connection);
+      session.setUserTransaction(new DefaultUserTransaction(connection));
+      session.setState(SessionState.RUNNING);
+      }
+    }
+  
+    public void stopSession() throws SystemException {
+      Session session = sessionThreadLocal.get();
+      if (session.getState() == SessionState.STOPPED) {
+        throw new IllegalStateException("Session is not running in thread for context " + contextName);
+      } else {
+        session.setState(SessionState.STOPPED);
+        ((DefaultUserTransaction) session.getUserTransaction()).close();
+        session.setUserTransaction(null);
+        sessionConnectionHandler.closeConnection(session.getConnection());
+        session.setConnection(null);
+      }
+    }
+  
   }
 
   private static class Session {
 
-	private Connection connection;
-	private UserTransaction userTransaction;
-	private SessionState state = SessionState.STOPPED;
-
-	public Connection getConnection() {
-	  return connection;
-	}
-
-	public void setConnection(Connection connection) {
-	  this.connection = connection;
-	}
-
-	public SessionState getState() {
-	  return state;
-	}
-
-	public void setState(SessionState state) {
-	  this.state = state;
-	}
-
-	public UserTransaction getUserTransaction() {
-	  return userTransaction;
-	}
-
-	public void setUserTransaction(UserTransaction userTransaction) {
-	  this.userTransaction = userTransaction;
-	}
+    private Connection connection;
+    private UserTransaction userTransaction;
+    private SessionState state = SessionState.STOPPED;
+  
+    public Connection getConnection() {
+      return connection;
+    }
+  
+    public void setConnection(Connection connection) {
+      this.connection = connection;
+    }
+  
+    public SessionState getState() {
+      return state;
+    }
+  
+    public void setState(SessionState state) {
+      this.state = state;
+    }
+  
+    public UserTransaction getUserTransaction() {
+      return userTransaction;
+    }
+  
+    public void setUserTransaction(UserTransaction userTransaction) {
+      this.userTransaction = userTransaction;
+    }
   }
 
   private static enum SessionState {
-	STOPPED, RUNNING
+    STOPPED, RUNNING
   }
 
   /**
@@ -247,94 +264,94 @@ public class SessionContextFactory {
    */
   protected static class DefaultUserTransaction implements UserTransaction {
 
-	private Connection connection;
+  private Connection connection;
 
-	private TransactionState transactionState;
+  private TransactionState transactionState;
 
-	/**
-	 * Constructs a DefaultUserTransaction object.
-	 *
-	 * @param connection  the current database connection
-	 */
-	public DefaultUserTransaction(Connection connection) {
-	  this.connection = connection;
-	  this.transactionState = TransactionState.NEW;
-	}
+  /**
+   * Constructs a DefaultUserTransaction object.
+   *
+   * @param connection  the current database connection
+   */
+  public DefaultUserTransaction(Connection connection) {
+    this.connection = connection;
+    this.transactionState = TransactionState.NEW;
+  }
 
-	public void begin() throws SystemException {
-	  if (transactionState == TransactionState.NEW) {
-		transactionState = TransactionState.IN_TRANSACTION;
-	  } else {
-		throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
-	  }
-	}
+  public void begin() throws SystemException {
+    if (transactionState == TransactionState.NEW) {
+    transactionState = TransactionState.IN_TRANSACTION;
+    } else {
+    throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
+    }
+  }
 
-	public void commit() throws SystemException, RollbackException {
-	  if (transactionState == TransactionState.NEW
-		  || transactionState == TransactionState.CLOSED) {
-		throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
-	  } else {
-		if (transactionState == TransactionState.IN_TRANSACTION_ROLLBACK) {
-		  transactionState = TransactionState.NEW;
-		  try {
-			connection.rollback();
-		  } catch (SQLException e) {
-			throw new SystemException(e);
-		  }
-		  throw new RollbackException("Transaction was rolled back");
-		} else {
-		  transactionState = TransactionState.NEW;
-		  try {
-			connection.commit();
-		  } catch (SQLException e) {
-			throw new SystemException(e);
-		  }
-		}
-	  }
-	}
+  public void commit() throws SystemException, RollbackException {
+    if (transactionState == TransactionState.NEW
+      || transactionState == TransactionState.CLOSED) {
+    throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
+    } else {
+    if (transactionState == TransactionState.IN_TRANSACTION_ROLLBACK) {
+      transactionState = TransactionState.NEW;
+      try {
+      connection.rollback();
+      } catch (SQLException e) {
+      throw new SystemException(e);
+      }
+      throw new RollbackException("Transaction was rolled back");
+    } else {
+      transactionState = TransactionState.NEW;
+      try {
+      connection.commit();
+      } catch (SQLException e) {
+      throw new SystemException(e);
+      }
+    }
+    }
+  }
 
-	public boolean isRollbackOnly() {
-	  if (transactionState == TransactionState.NEW
-		  || transactionState == TransactionState.CLOSED) {
-		throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
-	  } else {
-		return transactionState == TransactionState.IN_TRANSACTION_ROLLBACK;
-	  }
-	}
+  public boolean isRollbackOnly() {
+    if (transactionState == TransactionState.NEW
+      || transactionState == TransactionState.CLOSED) {
+    throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
+    } else {
+    return transactionState == TransactionState.IN_TRANSACTION_ROLLBACK;
+    }
+  }
 
-	public void rollback() throws SystemException {
-	  if (transactionState == TransactionState.NEW
-		  || transactionState == TransactionState.CLOSED) {
-		throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
-	  } else {
-		try {
-		  transactionState = TransactionState.NEW;
-		  connection.rollback();
-		} catch (SQLException e) {
-		  throw new SystemException(e);
-		}
-	  }
-	}
+  public void rollback() throws SystemException {
+    if (transactionState == TransactionState.NEW
+      || transactionState == TransactionState.CLOSED) {
+    throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
+    } else {
+    try {
+      transactionState = TransactionState.NEW;
+      connection.rollback();
+    } catch (SQLException e) {
+      throw new SystemException(e);
+    }
+    }
+  }
 
-	public void setRollbackOnly() throws SystemException {
-	  if (transactionState == TransactionState.NEW
-		  || transactionState == TransactionState.CLOSED) {
-		throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
-	  } else {
-		transactionState = TransactionState.IN_TRANSACTION_ROLLBACK;
-	  }
-	}
+  public void setRollbackOnly() throws SystemException {
+    if (transactionState == TransactionState.NEW
+      || transactionState == TransactionState.CLOSED) {
+    throw new IllegalStateException("Invalid state: Transaction is " + transactionState);
+    } else {
+    transactionState = TransactionState.IN_TRANSACTION_ROLLBACK;
+    }
+  }
 
-	/**
-	 * Closes the transaction.
-	 */
-	public void close() {
-	  transactionState = TransactionState.CLOSED;
-	}
+  /**
+   * Closes the transaction.
+   */
+  public void close() {
+    transactionState = TransactionState.CLOSED;
+  }
 
-	private static enum TransactionState {
-	  NEW, IN_TRANSACTION, IN_TRANSACTION_ROLLBACK, CLOSED
-	}
+  private static enum TransactionState {
+    NEW, IN_TRANSACTION, IN_TRANSACTION_ROLLBACK, CLOSED
+  }
 
   }
 }
