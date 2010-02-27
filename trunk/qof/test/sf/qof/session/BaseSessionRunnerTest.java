cf2fdef25d63ec9a53a11c191c181ad85e9c9b07
brunella <brunella@c553a772-16ad-418b-8912-1e50f42fc919>
2010-02-27 17:26:21 +0000

    Reformatting
    git-svn-id: http://svn.code.sf.net/p/qof-jdbc/code@107 c553a772-16ad-418b-8912-1e50f42fc919

--- trunk/qof/test/sf/qof/session/BaseSessionRunnerTest.java
+++ trunk/qof/test/sf/qof/session/BaseSessionRunnerTest.java
@@ -17,109 +17,109 @@ public class BaseSessionRunnerTest extends TestCase {
   
   private class MockDataSource implements DataSource {
 
-	public Connection getConnection() throws SQLException {
-	  Connection connection = MockConnectionFactory.getConnection();
-		log = ((MockConnectionData)connection).getLog();
-	  return connection;
-	}
+    public Connection getConnection() throws SQLException {
+      Connection connection = MockConnectionFactory.getConnection();
+        log = ((MockConnectionData)connection).getLog();
+      return connection;
+    }
 
-	public Connection getConnection(String username, String password) throws SQLException {
-	  return null;
-	}
+    public Connection getConnection(String username, String password) throws SQLException {
+      return null;
+    }
 
-	public PrintWriter getLogWriter() throws SQLException {
-	  return null;
-	}
+    public PrintWriter getLogWriter() throws SQLException {
+      return null;
+    }
 
-	public int getLoginTimeout() throws SQLException {
-	  return 0;
-	}
+    public int getLoginTimeout() throws SQLException {
+      return 0;
+    }
 
-	public void setLogWriter(PrintWriter out) throws SQLException {
-	}
+    public void setLogWriter(PrintWriter out) throws SQLException {
+    }
 
-	public void setLoginTimeout(int seconds) throws SQLException {
-	}
+    public void setLoginTimeout(int seconds) throws SQLException {
+    }
 
-	@SuppressWarnings("unused")
+    @SuppressWarnings("unused")
   public boolean isWrapperFor(Class<?> iface) throws SQLException {
-	  return false;
-	}
+      return false;
+    }
 
-	@SuppressWarnings("unused")
+    @SuppressWarnings("unused")
   public <T> T unwrap(Class<T> iface) throws SQLException {
-	  return null;
-	}
-	
+      return null;
+    }
+    
   }
   
   public void testSuccessDefaultContext() throws SystemException {
-	SessionContextFactory.setDataSource(new MockDataSource());
-	String result = new BaseSessionRunner<String>() {
-	  @Override
-	  protected String run(Connection connection, Object... arguments) throws SQLException {
-		assertEquals(1, arguments.length);
-		return "TEST1";
-	  }}.execute("TEST1");
-	assertEquals("TEST1", result);
-	assertEquals(3, log.size());
-	assertEquals("setAutoCommit(false)", log.get(0));
-	assertEquals("commit()", log.get(1));
-	assertEquals("close()", log.get(2));
+    SessionContextFactory.setDataSource(new MockDataSource());
+    String result = new BaseSessionRunner<String>() {
+      @Override
+      protected String run(Connection connection, Object... arguments) throws SQLException {
+        assertEquals(1, arguments.length);
+        return "TEST1";
+      }}.execute("TEST1");
+    assertEquals("TEST1", result);
+    assertEquals(3, log.size());
+    assertEquals("setAutoCommit(false)", log.get(0));
+    assertEquals("commit()", log.get(1));
+    assertEquals("close()", log.get(2));
   }
   
   public void testSuccessNamedContext() throws SystemException {
-	SessionContextFactory.setDataSource("MY_CONTEXT", new MockDataSource());
-	String result = new BaseSessionRunner<String>("MY_CONTEXT") {
-	  @Override
-	  protected String run(Connection connection, Object... arguments) throws SQLException {
-		assertEquals(1, arguments.length);
-		return "TEST2";
-	  }}.execute("TEST2");
-	assertEquals("TEST2", result);
-	assertEquals(3, log.size());
-	assertEquals("setAutoCommit(false)", log.get(0));
-	assertEquals("commit()", log.get(1));
-	assertEquals("close()", log.get(2));
+    SessionContextFactory.setDataSource("MY_CONTEXT", new MockDataSource());
+    String result = new BaseSessionRunner<String>("MY_CONTEXT") {
+      @Override
+      protected String run(Connection connection, Object... arguments) throws SQLException {
+        assertEquals(1, arguments.length);
+        return "TEST2";
+      }}.execute("TEST2");
+    assertEquals("TEST2", result);
+    assertEquals(3, log.size());
+    assertEquals("setAutoCommit(false)", log.get(0));
+    assertEquals("commit()", log.get(1));
+    assertEquals("close()", log.get(2));
   }
   
   public void testFailureDefaultContext() throws SystemException {
-	SessionContextFactory.setDataSource(new MockDataSource());
-	try {
-	  new BaseSessionRunner<String>() {
-		@Override
-		protected String run(Connection connection, Object... arguments)
-			throws SQLException {
-		  throw new SQLException("failed");
-		}
-	  }.execute();
-	  fail("exception expected");
-	} catch (SystemException s) {
-	  assertEquals("failed", s.getCause().getMessage());
-	}
-	assertEquals(3, log.size());
-	assertEquals("setAutoCommit(false)", log.get(0));
-	assertEquals("rollback()", log.get(1));
-	assertEquals("close()", log.get(2));
+    SessionContextFactory.setDataSource(new MockDataSource());
+    try {
+      new BaseSessionRunner<String>() {
+        @Override
+        protected String run(Connection connection, Object... arguments)
+            throws SQLException {
+          throw new SQLException("failed");
+        }
+      }.execute();
+      fail("exception expected");
+    } catch (SystemException s) {
+      assertEquals("failed", s.getCause().getMessage());
+    }
+    assertEquals(3, log.size());
+    assertEquals("setAutoCommit(false)", log.get(0));
+    assertEquals("rollback()", log.get(1));
+    assertEquals("close()", log.get(2));
   }
   
   public void testForcedRollbackDefaultContext() throws SystemException {
-	SessionContextFactory.setDataSource(new MockDataSource());
-	new BaseSessionRunner<Void>() {
-	  @Override
-	  protected Void run(Connection connection, Object... arguments)
-		  throws SQLException {
-		try {
-		  SessionContextFactory.getContext().getUserTransaction()
-			  .setRollbackOnly();
-		} catch (SystemException e) {
-		}
-		return null;
-	  }
-	}.execute();
-	assertEquals(3, log.size());
-	assertEquals("setAutoCommit(false)", log.get(0));
-	assertEquals("rollback()", log.get(1));
-	assertEquals("close()", log.get(2));
+    SessionContextFactory.setDataSource(new MockDataSource());
+    new BaseSessionRunner<Void>() {
+      @Override
+      protected Void run(Connection connection, Object... arguments)
+          throws SQLException {
+        try {
+          SessionContextFactory.getContext().getUserTransaction()
+              .setRollbackOnly();
+        } catch (SystemException e) {
+        }
+        return null;
+      }
+    }.execute();
+    assertEquals(3, log.size());
+    assertEquals("setAutoCommit(false)", log.get(0));
+    assertEquals("rollback()", log.get(1));
+    assertEquals("close()", log.get(2));
   }
 }
