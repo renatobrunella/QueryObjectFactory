39045a491ad0658c345cfc2ce4b8d389aab4f245
brunella <brunella@c553a772-16ad-418b-8912-1e50f42fc919>
2010-03-29 10:22:10 +0000

    Test coverage
    git-svn-id: http://svn.code.sf.net/p/qof-jdbc/code@134 c553a772-16ad-418b-8912-1e50f42fc919

--- trunk/qof/test/sf/qof/bundle/QueryObjectFactoryActivatorTest.java
+++ trunk/qof/test/sf/qof/bundle/QueryObjectFactoryActivatorTest.java
@@ -7,6 +7,8 @@ import sf.qof.Query;
 import sf.qof.QueryObjectFactoryService;
 import sf.qof.adapter.ClobAdapter;
 import sf.qof.adapter.CommonAdapterRegistrar;
+import sf.qof.customizer.DefaultCustomizer;
+import sf.qof.dialect.HSQLDbDialect;
 import junit.framework.TestCase;
 
 public class QueryObjectFactoryActivatorTest extends TestCase {
@@ -43,7 +45,7 @@ public class QueryObjectFactoryActivatorTest extends TestCase {
     assertNull(activator.delegator);
   }
 
-  public void testUseService() throws Exception {
+  public void testUseServiceCreateQueryObject1() throws Exception {
     TestableQueryObjectFactoryActivator activator = new TestableQueryObjectFactoryActivator();
     MockBundleContext context = new MockBundleContext();
     MockBundle bundle = new MockBundle();
@@ -58,11 +60,110 @@ public class QueryObjectFactoryActivatorTest extends TestCase {
     CommonAdapterRegistrar.registerCommonAdapters();
     new CommonAdapterRegistrar();
   }
+  
+  public void testUseServiceCreateQueryObject2() throws Exception {
+    TestableQueryObjectFactoryActivator activator = new TestableQueryObjectFactoryActivator();
+    MockBundleContext context = new MockBundleContext();
+    MockBundle bundle = new MockBundle();
+    activator.start(context);
+    QueryObjectFactoryService service = (QueryObjectFactoryService) activator.getService(bundle, null);
+    AbstractDao1 dao = service.createQueryObject(AbstractDao1.class, "test");
+    assertNotNull(dao);
+    assertEquals("test", dao.argument);
+    activator.ungetService(bundle, null, service);
+    activator.stop(context);
+  }
+  
+  public void testUseServiceCreateQueryObjectFromSuperClass1() throws Exception {
+    TestableQueryObjectFactoryActivator activator = new TestableQueryObjectFactoryActivator();
+    MockBundleContext context = new MockBundleContext();
+    MockBundle bundle = new MockBundle();
+    activator.start(context);
+    QueryObjectFactoryService service = (QueryObjectFactoryService) activator.getService(bundle, null);
+    DaoBase dao = service.createQueryObjectFromSuperClass(DaoBase.class, Base.class);
+    assertNotNull(dao);
+    assertEquals("base", ((Base) dao).base);
+    activator.ungetService(bundle, null, service);
+    activator.stop(context);
+  }
+  
+  public void testUseServiceCreateQueryObjectFromSuperClass2() throws Exception {
+    TestableQueryObjectFactoryActivator activator = new TestableQueryObjectFactoryActivator();
+    MockBundleContext context = new MockBundleContext();
+    MockBundle bundle = new MockBundle();
+    activator.start(context);
+    QueryObjectFactoryService service = (QueryObjectFactoryService) activator.getService(bundle, null);
+    DaoBase dao = service.createQueryObjectFromSuperClass(DaoBase.class, Base.class, "argument");
+    assertNotNull(dao);
+    assertEquals("argument", ((Base) dao).base);
+    activator.ungetService(bundle, null, service);
+    activator.stop(context);
+  }
+  
+  public void testUseServiceMapper2() throws Exception {
+    TestableQueryObjectFactoryActivator activator = new TestableQueryObjectFactoryActivator();
+    MockBundleContext context = new MockBundleContext();
+    MockBundle bundle = new MockBundle();
+    activator.start(context);
+    QueryObjectFactoryService service = (QueryObjectFactoryService) activator.getService(bundle, null);
+    assertFalse(service.isMapperRegistered("myclob"));
+    service.registerMapper("myclob", new ClobAdapter());
+    assertTrue(service.isMapperRegistered("myclob"));
+    service.unregisterMapper("myclob");
+    assertFalse(service.isMapperRegistered("myclob"));
+    activator.ungetService(bundle, null, service);
+    activator.stop(context);
+  }
+  
+  public void testCustomizers() throws Exception {
+    TestableQueryObjectFactoryActivator activator = new TestableQueryObjectFactoryActivator();
+    MockBundleContext context = new MockBundleContext();
+    MockBundle bundle = new MockBundle();
+    activator.start(context);
+    QueryObjectFactoryService service = (QueryObjectFactoryService) activator.getService(bundle, null);
+    try {
+      service.setCustomizer(new DefaultCustomizer());
+      service.setDefaultCustomizer();
+      service.setSQLDialect(new HSQLDbDialect());
+    } catch (Exception e) {
+      fail("failed");
+    }
+    activator.ungetService(bundle, null, service);
+    activator.stop(context);
+  }
 
   private class TestableQueryObjectFactoryActivator extends QueryObjectFactoryActivator { }
   
   private interface Dao extends BaseQuery {
     @Query(sql = "select clob {myclob%%} from test where s = {%1}")
     String test(String s) throws SQLException;
+  }
+  
+  private static abstract class AbstractDao1 implements BaseQuery {
+    public String argument;
+
+    @SuppressWarnings("unused")
+    public AbstractDao1(String argument) {
+      this.argument = argument;
+    }
+    
+    @Query(sql = "select s {%%} from test where s = {%1}")
+    public abstract String test(String s) throws SQLException;
+  }
+  
+  private interface DaoBase extends BaseQuery {
+    @Query(sql = "select s {%%} from test where s = {%1}")
+    String test(String s) throws SQLException;
+  }
+
+  public static class Base {
+    public String base;
+    public Base() {
+      base = "base";
+    }
+    
+    public Base(String argument) {
+      base = argument;
+    }
   }
 }
